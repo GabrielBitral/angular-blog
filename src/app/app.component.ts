a47abeafe36710dc356456c9cc201743118f59d4
Gabriel Fabiano <gabrielfabianochaves@gmail.com>
2024-01-27 16:22:49 -0300

    :lipstick: stylized core components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TitleBlogComponent } from './components/title-blog/title-blog.component';
-import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
-import { BigCardComponent } from './components/big-card/big-card.component';
-import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleBlogComponent, MenuBarComponent, BigCardComponent, SmallCardComponent],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
