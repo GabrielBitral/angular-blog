d240d79600103d3875fdf900ede216ec88e97a4b
Gabriel Fabiano <gabrielfabianochaves@gmail.com>
2024-01-26 21:26:40 -0300

    :lipstick: feat add new components and your styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TitleBlogComponent } from './components/title-blog/title-blog.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TitleBlogComponent, MenuBarComponent, BigCardComponent, SmallCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
